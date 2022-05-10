66667e021b126f3d0dcb01b73144fd0266047d81
Uzen Huang <uzen.huang@gmail.com>
2022-05-10 22:36:49 +0800

    first test

---
+++ scraper.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import wget
+import zipfile
+import requests
+import os
+import sys
+from bs4 import BeautifulSoup
+
+URL = sys.argv[1]
+page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, "html.parser")
+
+images = soup.findAll("img", class_="viewer__img swiper-lazy")
+
+url = []
+for i in images:
+    if i.get('src'):
+        url.append(i['src'])
+    else:
+        url.append(i['data-src'])
+
+with zipfile.ZipFile("chapter.cbz", 'w') as zip_f:
+    for i in range(len(url)):
+        print("Page " + str(i) + ": " + url[i])
+        filename = str(i)+".jpg"
+        wget.download(url[i], filename)
+        zip_f.write(filename)
+        print()
+        os.remove(filename)
